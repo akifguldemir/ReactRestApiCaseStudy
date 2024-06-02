7681be2855fa8d1103b6d839781716d28984de8c
akifguldemir <akifguldemir@gmail.com>
2024-06-02 17:47:08 +0300

    delete and confirmdelete methds added

--- reactCaseStudy/src/components/Dashboard.jsx
+++ reactCaseStudy/src/components/Dashboard.jsx
@@ -24,20 +24,34 @@ const Dashboard = () => {
     fetchPosts();
   }, []);
 
-  const handleDelete = async (postId) => { alert(postId)
-      await axios.delete(`http://localhost:8080/posts/${postId}`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
+  const handleDelete = async (postId) => {
+      await axios.delete(`http://localhost:8080/api/posts/${postId}`, {
+        // headers: {
+        //   Authorization: `Bearer ${localStorage.getItem('token')}`,
+        // },
       }).then(function (response) {
-        console.log(response);
         setPosts(posts.filter((post) => post.id !== postId));
-        setPosts(response.data);
+        toast.success(response.data.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
       })
       .catch(function (error) {
         console.log(error);
       });
-    
+  };
+
+  const confirmDelete = (postId) => {
+    const confirmed = window.confirm('Silmek istiyor musunuz?');
+    if (confirmed) {
+      handleDelete(postId);
+    }
   };
 
   return (
@@ -60,7 +74,7 @@ const Dashboard = () => {
             <td>{post.title}</td>
             <td>{post.body}</td>
             <td>
-              <button type="button" className="btn btn-danger" onClick={() => handleDelete(post.id)}>Sil</button>
+              <button type="button" className="btn btn-danger" onClick={() => confirmDelete(post.id)}>Sil</button>
             </td>
           </tr>
         ))}
